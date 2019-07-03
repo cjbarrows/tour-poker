fa048a9937d9b9aa871e8055b2b10d381fd9183f
Charlie Barrows <charlie.barrows@gmail.com>
2019-07-03 17:21:52 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { hand: null };
+
+  async componentDidMount() {
+    const res = await fetch('/player');
+    const json = await res.json();
+    this.setState({ hand: json })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Player 1's Hand</h1>
+        {this.state.hand && this.state.hand.map(card => (<p>{card.name}</p>))}
+      </div>
+    );
+  }
 }
 
 export default App;
