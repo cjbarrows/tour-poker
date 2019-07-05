d24e489794ff6b0db2d1e337ca9340b805f5004e
Charlie Barrows <charlie.barrows@gmail.com>
2019-07-05 09:49:10 -0400

    fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(proxy('/player',
     {
-      target: 'http://tour-poker-server.herokuapp.com/',
+      target: 'https://tour-poker-server.herokuapp.com/player',
       changeOrigin: true
     }
   ));
