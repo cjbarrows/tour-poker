bc2f028e9eebdc9b40b4cd74927122582f8032dc
Charlie Barrows <charlie.barrows@gmail.com>
2019-07-05 09:37:52 -0400

    try with api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  app.use(proxy('/player',
+  app.use(proxy('/api',
     { target: 'https://tour-poker-server.herokuapp.com/' }
   ));
 }
