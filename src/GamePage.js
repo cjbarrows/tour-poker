59603a30482890e7ac5a17af0486ed0e02056927
Charlie Barrows <charlie.barrows@gmail.com>
2019-07-12 09:16:48 -0400

    react-measure

--- src/GamePage.js
+++ src/GamePage.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import { withContentRect } from 'react-measure';
 
 import Hands from './Hands';
 import Controls from './controls/Controls';
@@ -7,29 +8,24 @@ import WinnerAlert from './WinnerAlert';
 
 import './GamePage.css';
 
-class GamePage extends Component {
-  render() {
-    const { turn, gameWinner } = this.props;
+const GamePage = withContentRect('bounds')(props => {
+  const { turn, gameWinner, measureRef, contentRect } = props;
 
-    const {
-      user: { username: loggedIn }
-    } = JSON.parse(localStorage.getItem('user'));
+  const {
+    user: { username: loggedIn }
+  } = JSON.parse(localStorage.getItem('user'));
 
-    return (
-      <div className="game-page">
-        <Hands loggedIn={loggedIn} selectorFunction={this.setSelectedCards} />
-        {turn === loggedIn && <Controls loggedIn={loggedIn} />}
-        {gameWinner && Object.entries(gameWinner).length > 0 ? (
-          <WinnerAlert winner={gameWinner.name} hand={gameWinner.hand} />
-        ) : null}
-      </div>
-    );
-  }
-
-  setSelectedCards(cards) {
-    console.log(cards);
-  }
-}
+  return (
+    <div ref={measureRef} className="game-page">
+      <pre>{JSON.stringify(contentRect, null, 2)}</pre>
+      <Hands loggedIn={loggedIn} />
+      {turn === loggedIn && <Controls loggedIn={loggedIn} />}
+      {gameWinner && Object.entries(gameWinner).length > 0 ? (
+        <WinnerAlert winner={gameWinner.name} hand={gameWinner.hand} />
+      ) : null}
+    </div>
+  );
+});
 
 const mapStateToProps = state => ({
   turn: state.gameReducer.turn,
